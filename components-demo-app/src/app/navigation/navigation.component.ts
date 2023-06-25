2187d20a5ef861a5716bd624a52bf6b99e4dfc1a
Alexander Alexiev <alexandervalentinov.a@gmail.com>
2023-06-25 17:05:35 +0300

    ngif and ngfor examples

--- components-demo-app/src/app/navigation/navigation.component.ts
+++ components-demo-app/src/app/navigation/navigation.component.ts
@@ -6,8 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
-  handleClick() {
-    console.log('clicked');
+  title = 'hide';
+  toggle = false;
+  isActive = false;
+
+  activeUsers = [
+    {name: 'Mitko', age: 21}, 
+    {name: 'Pesho', age: 22}, 
+    {name: 'Mariika', age: 13}, 
+    {name: 'Katya', age: 18}
+  ];
+
+  handleClick(): void {
+    this.toggle = !this.toggle;
+    this.isActive = !this.isActive;
   }
 }
 
