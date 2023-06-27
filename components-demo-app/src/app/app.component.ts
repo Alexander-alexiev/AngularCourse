28ad285894683a5d7239c263b76681fae67f6481
Alexander Alexiev <alexandervalentinov.a@gmail.com>
2023-06-27 23:03:16 +0300

    added some life hooks and input/output testing.

--- components-demo-app/src/app/app.component.ts
+++ components-demo-app/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'components-demo-app';
+
+  users = [
+    { name: 'Mitko', age: true },
+    { name: 'Pesho', age: true },
+    { name: 'Mariika', age: false },
+    { name: 'Katya', age: false }
+  ];
+  outputChildHandler(){
+    console.log('onOUtputFromChild');
+  }
 }
--- components-demo-app/src/app/navigation/navigation.component.ts
+++ components-demo-app/src/app/navigation/navigation.component.ts
@@ -1,25 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit, OnChanges {
+  @Input('title') titleFromApp: string = '';
+  @Input('activeUsers') activeUsers: {name:string, age:boolean}[] = [];
+  @Output() onChildOutput = new EventEmitter<boolean>();
+
   title = 'hide';
   toggle = false;
   isActive = false;
+  inputValue = 'Hello!';
 
-  activeUsers = [
-    {name: 'Mitko', age: 21}, 
-    {name: 'Pesho', age: 22}, 
-    {name: 'Mariika', age: 13}, 
-    {name: 'Katya', age: 18}
-  ];
+  ngOnInit() {
+    console.log('navigation -> initialization');
+  }
 
-  handleClick(): void {
+  ngOnAfterViewInit() {
+    console.log('navigation -> after view init');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('navigation -> changes');
+  }
+  
+
+  handleClick(event: Event): void {
     this.toggle = !this.toggle;
     this.isActive = !this.isActive;
+    this.onChildOutput.emit(this.isActive);
   }
 }
 
